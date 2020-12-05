92799591d913de317d0059fadc1485ef7223154d
yuuki <58385540+yuukixdev@users.noreply.github.com>
2020-12-05 14:47:07 +0700

    Update analytics.js

--- assets/javascript/analytics.js
+++ assets/javascript/analytics.js
@@ -3,5 +3,5 @@
 s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
 (window,document,'ga','script','https://www.google-analytics.com/analytics.js');
 
-ga('create', 'UA-122247573-1', 'https://yuukixdev.github.io/dd0sware.tk/');
+ga('create', 'UA-122247573-1', 'https://yuukixdev.github.io/');
 ga('send', 'pageview');
