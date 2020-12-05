6ad9073d8330ebd3794cb5559c5e73a44a4549ba
yuuki <58385540+yuukixdev@users.noreply.github.com>
2020-12-05 14:06:00 +0700

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+// Made by Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
+// Credits to expl0it, shellcode.team
+// GitHub: https://github.com/Lumm1t/obnoxious.club
+
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.12;
+  musicFadeIn = 4000;
+  skippedIntro = false;
+  backgroundToggler = false;
+  shouldIgnoreVideo = false;
+  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  brandDescription = ['Four competent developers', 'Easy To Use', 'Full Lua Level 6 - 7', 'We have always been dedicated to the security of our users', 'Rainfall gives you the ability to execute scripts at will. If ROBLOX can run it, so can Rainfall.', 'Press - Rainfall Development to Download'];
+
+  titleChanger = (text, delay) => {
+    if (!text) return;
+
+    delay = delay || 2000;
+
+    let counter = 0;
+
+    setInterval(() => {
+      if (counter < text.length) document.title = text[counter++];
+      else document.title = text[(counter = 0)];
+    }, delay);
+  };
+
+  iconChanger = (urls, delay) => {
+    if (!urls) return;
+
+    delay = delay || 2000;
+
+    let counter = 0;
+
+    setInterval(() => {
+      if (counter < urls.length) {
+        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = urls[counter];
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+      } else counter = 0;
+
+      ++counter;
+    }, delay);
+  };
+}
+
+const app = new _app();
